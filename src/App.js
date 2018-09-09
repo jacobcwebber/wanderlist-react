2fb512615d07d0d2fdaf249087dc739713a1dae5
Jacob Webber <jacobcwebber@gmail.com>
2018-09-09 14:55:14 -0400

    User signs up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import SignupPage from "./components/pages/SignupPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
 
 const App = () => (
   <div className="ui container">
-    <Switch className="ui container">
+    <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/login" exact component={LoginPage} />
+      <GuestRoute path="/login" exact component={LoginPage} />
+      <GuestRoute path="/signup" exact component={SignupPage} />
+      <UserRoute path="/dashboard" exact component={DashboardPage} />
     </Switch>
   </div>
 );
